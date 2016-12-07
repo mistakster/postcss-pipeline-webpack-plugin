5c61fb1a026ffe26c7f9c8dfcfee2cce28005b24
Vladimir Kuznetsov <mistakster@gmail.com>
2016-12-07 10:20:07 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,1 @@
+require('./lib/postcss-pipeline-webpack-plugin');
---
+++ tests/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const PostCssPipelineWebpackPlugin = require('./postcss-pipeline-webpack-plugin');
+const criticalSplit = require('postcss-critical-split');
+const csso = require('postcss-csso');
+
+module.exports = {
+	entry: './src/index.css',
+
+	output: {
+		path: path.resolve('./dest/'),
+		filename: '[name].js'
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract('style', 'css?sourceMap&-autoprefixer')
+			}
+		]
+	},
+
+	plugins: [
+		new ExtractTextPlugin('styles.css'),
+		new PostCssPipelineWebpackPlugin({
+			suffix: 'critical',
+			pipeline: [
+				criticalSplit({
+					output: criticalSplit.output_types.CRITICAL_CSS
+				})
+			]
+		}),
+		new PostCssPipelineWebpackPlugin({
+			suffix: 'min',
+			pipeline: [
+				csso({
+					restructure: false
+				})
+			]
+		})
+	]
+};
