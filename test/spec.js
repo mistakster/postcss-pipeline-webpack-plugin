eefacb25f261e6d70850e453a2fbf33f58c5c22e
Vladimir Kuznetsov <mistakster@gmail.com>
2016-12-19 17:30:54 +0500

    added more test

--- test/spec.js
+++ test/spec.js
@@ -1,11 +1,13 @@
+const criticalSplit = require('postcss-critical-split');
+const csso = require('postcss-csso');
 const PostCssPipelineWebpackPlugin = require('../lib/postcss-pipeline-webpack-plugin');
 const RawSource = require('webpack-sources').RawSource;
 const assert = require('assert');
 const fs = require('fs');
 
-function getFixture() {
+function readFile(name) {
   return new Promise((resolve, reject) => {
-    fs.readFile('./test/fixtures/index.css', 'utf8', (err, data) => {
+    fs.readFile(name, 'utf8', (err, data) => {
       if (err) {
         reject(err);
       } else {
@@ -15,9 +17,13 @@ function getFixture() {
   });
 }
 
+function getFixture() {
+  return readFile('./test/fixtures/index.css');
+}
+
 describe('PostCss Pipeline Webpack Plugin', function () {
-  it('should leave given CSS untouched with default options', function (done) {
-    getFixture()
+  it('should leave given CSS untouched with default options', function () {
+    return getFixture()
       .then(source => {
         const plugin = new PostCssPipelineWebpackPlugin();
 
@@ -39,7 +45,159 @@ describe('PostCss Pipeline Webpack Plugin', function () {
           compilation.assets[keys[0]].source().toString(),
           compilation.assets[keys[1]].source().toString()
         );
+      });
+  });
+
+  it('should generate properly named files', function () {
+    const plugin = new PostCssPipelineWebpackPlugin({
+      suffix: 'min'
+    });
+
+    return plugin
+      .generate({
+        assets: {
+          './index.css': new RawSource('')
+        }
       })
-      .then(done, done);
+      .then(compilation => {
+        const keys = Object.keys(compilation.assets);
+
+        assert.deepStrictEqual(keys, [
+          './index.css',
+          './index.min.css'
+        ]);
+      });
+  });
+
+  it('should filter files to process', function () {
+    const plugin = new PostCssPipelineWebpackPlugin({
+      predicate: name => /foobar.css$/.test(name)
+    });
+
+    return plugin
+      .generate({
+        assets: {
+          './index.js': new RawSource(''),
+          './index.css': new RawSource(''),
+          './foobar.css': new RawSource('')
+        }
+      })
+      .then(compilation => {
+        const keys = Object.keys(compilation.assets);
+
+        assert.deepStrictEqual(keys, [
+          './index.js',
+          './index.css',
+          './foobar.css',
+          './foobar.processed.css'
+        ]);
+      });
+  });
+
+  it('should process SourceMaps as well', function () {
+    const plugin = new PostCssPipelineWebpackPlugin({
+      map: {
+        inline: false
+      }
+    });
+
+    return plugin
+      .generate({
+        assets: {
+          './index.css': new RawSource('')
+        }
+      })
+      .then(compilation => {
+        const keys = Object.keys(compilation.assets);
+
+        assert.deepStrictEqual(keys, [
+          './index.css',
+          './index.processed.css',
+          './index.processed.css.map'
+        ]);
+
+        assert.strictEqual(
+          compilation.assets[keys[1]].source().toString(),
+          '\n/*# sourceMappingURL=index.processed.css.map */'
+        );
+      });
+  });
+
+  it('should pass sources through pipeline correctly', function () {
+    return getFixture()
+      .then(source => ({
+        assets: {
+          './index.css': source
+        }
+      }))
+      .then(compilation => {
+        const plugin = new PostCssPipelineWebpackPlugin({
+          suffix: 'critical',
+          pipeline: [
+            criticalSplit({
+              output: criticalSplit.output_types.CRITICAL_CSS
+            })
+          ]
+        });
+
+        return plugin.generate(compilation);
+      })
+      .then(compilation => {
+        const plugin = new PostCssPipelineWebpackPlugin({
+          suffix: 'min',
+          pipeline: [
+            csso({
+              restructure: false
+            })
+          ],
+          map: {
+            inline: false
+          }
+        });
+
+        return plugin.generate(compilation);
+      })
+      .then(compilation => {
+        const keys = Object.keys(compilation.assets);
+
+        assert.deepStrictEqual(keys, [
+          './index.css',
+          './index.critical.css',
+          './index.min.css',
+          './index.min.css.map',
+          './index.critical.min.css',
+          './index.critical.min.css.map',
+        ]);
+
+        const fixtures = [
+          './test/fixtures/index.css',
+          './test/fixtures/index.critical.css',
+          './test/fixtures/index.min.css',
+          './test/fixtures/index.critical.min.css'
+        ];
+
+        return Promise.all(fixtures.map(readFile))
+          .then(files => {
+            assert.strictEqual(
+              compilation.assets[keys[0]].source().toString().replace(/\r\n/g, '\n'),
+              files[0].source().toString().replace(/\r\n/g, '\n')
+            );
+
+            assert.strictEqual(
+              compilation.assets[keys[1]].source().toString().replace(/\r\n/g, '\n'),
+              files[1].source().toString().replace(/\r\n/g, '\n')
+            );
+
+            assert.strictEqual(
+              compilation.assets[keys[2]].source().toString().replace(/\r\n/g, '\n'),
+              files[2].source().toString().replace(/\r\n/g, '\n')
+            );
+
+            assert.strictEqual(
+              compilation.assets[keys[4]].source().toString().replace(/\r\n/g, '\n'),
+              files[3].source().toString().replace(/\r\n/g, '\n')
+            );
+          });
+      });
   });
 });
